4804df2ad4bc2dd674eadee580c86841bdcce837
Caique Araujo <caique@studiopiggly.com.br>
2021-08-17 10:16:32 -0300

    PglySyncIconButton, PglyAsyncIconButton

--- src/lib-components/index.ts
+++ src/lib-components/index.ts
@@ -8,12 +8,14 @@ export { default as PglyToaster } from './components/PglyToaster.vue';
 
 // Elements
 export { default as PglyAsyncButton } from './elements/PglyAsyncButton.vue';
+export { default as PglyAsyncIconButton } from './elements/PglyAsyncIconButton.vue';
 export { default as PglyBadge } from './elements/PglyBadge.vue';
 export { default as PglyExplorer } from './elements/PglyExplorer.vue';
 export { default as PglyLinkButton } from './elements/PglyLinkButton.vue';
 export { default as PglyNotification } from './elements/PglyNotification.vue';
 export { default as PglySpinner } from './elements/PglySpinner.vue';
 export { default as PglySyncButton } from './elements/PglySyncButton.vue';
+export { default as PglySyncIconButton } from './elements/PglySyncIconButton.vue';
 export { default as PglyToast } from './elements/PglyToast.vue';
 
 // Forms
